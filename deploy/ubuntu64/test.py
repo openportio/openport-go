d420f7f3d72f2097f587d21bf65ef4535d65ec91
Jan De Bleser <jandebleser@gmail.com>
2018-09-02 22:01:10 +0200

    Added vagrant file and test for rebooting

---
+++ deploy/ubuntu64/test.py
@@ -0,0 +1,29 @@
+import subprocess
+from time import sleep
+from unittest import TestCase
+
+
+def run_command(cmd):
+    output = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
+    print(output)
+    return output
+
+
+class VagrantTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super(VagrantTest, cls).setUpClass()
+        run_command('cp ../../debian/openport_*-1_amd64.deb .')
+        run_command("vagrant up")
+
+    @classmethod
+    def tearDownClass(cls):
+        super(VagrantTest, cls).tearDownClass()
+        run_command("vagrant destroy -f")
+
+    def test_restart_on_reboot(self):
+        sleep(10)
+        run_command("vagrant ssh -c 'sudo reboot' || true")
+        sleep(10)
+        output = run_command("vagrant ssh -c 'ps aux|grep openport|grep -v grep'")
+        self.assertIn('openport 22', output)
