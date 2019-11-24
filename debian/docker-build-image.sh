9b77b60a1b2276288a2245442a4abc076e9a619c
Jan De Bleser <jandebleser@gmail.com>
2019-11-24 21:16:11 +0100

    Trying to fix arm distribution

--- debian/docker-build-image.sh
+++ debian/docker-build-image.sh
@@ -6,10 +6,13 @@ MACHINE=$(uname -m)
 DOCKERARGS=
 if [ "$MACHINE" == "armv7l" ] ; then
 	DOCKERARGS="--build-arg FROMIMAGE=resin/rpi-raspbian:jessie"
-
+	EXTRATAG="-tmp"
 fi
 export DOCKER_API_VERSION=1.23
 
 cd ../
-docker build $DOCKERARGS -t jandebleser/openport-distribution .
+docker build $DOCKERARGS -t jandebleser/openport-distribution${EXTRATAG} .
 
+if [ "$MACHINE" == "armv7l" ] ; then
+    docker build -f Dockerfile-arm -t jandebleser/openport-distribution .
+fi
