0e1e0a07ebacb9cbe0ed448cfa2f1db925980edb
Jan De Bleser <jan.debleser@restore.eu>
2021-12-13 17:20:16 +0100

    Fixing armhf packaging.

--- docker_compile_all.sh
+++ docker_compile_all.sh
@@ -3,8 +3,8 @@ set -ex
 cd "$(dirname "$0")"
 ./compile.sh
 ./docker_compile.sh amd64
-./docker_compile.sh arm64
-./docker_compile.sh armhf
-./docker_compile.sh windows-amd64
+#./docker_compile.sh armhf  # Segmentation fault on pi zero
 ./docker_compile.sh armv6
+./docker_compile.sh arm64
+./docker_compile.sh windows-amd64
 # ./docker_compile.sh snapcraft
