a75410bfeccf7cdda30f4b53bff6d76b3cb80f76
Jan De Bleser <jan.debleser@restore.eu>
2022-12-05 16:20:20 +0100

    Fixing TTY issue 3

--- docker_compile.sh
+++ docker_compile.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set -ex
 cd "$(dirname "$0")"
+INTERACTIVE=$([ -t 0 ] && echo "-t")
+
 
 ARCH=${1:-amd64}
 
 docker build . -f Dockerfile-$ARCH -t openport-go-$ARCH
-docker run -it -v $(pwd):/app openport-go-$ARCH bash -c 'cp openport* /app'
+docker run -i $INTERACTIVE -v $(pwd):/app openport-go-$ARCH bash -c 'cp openport* /app'
