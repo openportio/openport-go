bd092246ec3e8e346e0371ffc5854ab752db3a44
Jan De Bleser <jandebleser@gmail.com>
2024-05-21 13:25:53 +0200

    Fixing permissions of compilation output.

--- docker_compile.sh
+++ docker_compile.sh
@@ -6,4 +6,4 @@ cd "$(dirname "$0")"
 ARCH=${1:-amd64}
 
 docker build . -f Dockerfile-$ARCH -t openport-go-$ARCH
-docker run -i $INTERACTIVE -v $(pwd):/app openport-go-$ARCH bash -c 'cp /openport* /app'
+docker run -i $INTERACTIVE --user=$(id -u):$(id -g) -v $(pwd):/app openport-go-$ARCH bash -c 'cp /openport* /app'
--- test_docker_compilations.sh
+++ test_docker_compilations.sh
@@ -3,19 +3,19 @@ set -ex
 
 # amd64
 ./docker_compile.sh amd64
-./openport-amd64 --help
+./openport-amd64 selftest
 
 # armv6
 ./docker_compile.sh armv6
 scp openport-armv6 pi-zero:
-ssh pi-zero ./openport-armv6 --help
+ssh pi-zero ./openport-armv6 selftest
 
 # armv7
 ./docker_compile.sh armv7
 scp openport-armv7 router:
-ssh router ./openport-armv7 --help
+ssh router ./openport-armv7 selftest
 
 # arm64
 ./docker_compile.sh arm64
 scp openport-arm64 mk4:
-ssh mk4 ./openport-arm64 --help
+ssh mk4 ./openport-arm64 selftest
